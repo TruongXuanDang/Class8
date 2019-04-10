0363fdca78fbd3a30b07a56e7dcf76b2bcf7f520
Unknown <truonghusk17@gmail.com>
2019-04-10 23:31:57 +0700

    Bai4XML

---
+++ Bai4XML/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Stage mainStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Main.mainStage = primaryStage;
+
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/weather.fxml"));
+        Scene scene = new Scene(root,600,400);
+        primaryStage.setTitle("Weather app");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
